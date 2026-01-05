8311808cd661c1843103670c1413389148ea68af
Rahul Repal <rahulrepal@MacBookAir.bbrouter>
2026-01-05 18:56:38 +0530

    Fix major issue in Config

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,6 +9,6 @@ class Config:
     DEBUG = ENV == "local"
     SECRET_KEY = os.environ["SECRET_KEY"]
     JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
-    JWT_ACCESS_TOKEN_EXPIRES = os.environ["JWT_ACCESS_TOKEN_EXPIRES"]
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.environ["JWT_ACCESS_TOKEN_EXPIRES"])
     CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
     MONGO_URL = os.environ["MONGO_URL"]
