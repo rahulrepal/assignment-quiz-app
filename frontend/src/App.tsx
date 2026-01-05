a8e25283810ad30bea5fd0449e4650f344480943
Rahul Repal <rahulrepal@MacBookAir.bbrouter>
2026-01-05 19:49:16 +0530

    Add basic code for routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import { Button } from "@/components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProtectedRoute } from "./components/shared/ProtectedRoute";
 
 function App() {
-  return <Button className="m-4">Click Me</Button>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Listing page</h1>} />
+        <Route path="/quizzes/:quizId" element={<h1>Quiz page</h1>} />
+        <Route path="/quizzes/:quizId/result" element={<h1>Result page</h1>} />
+        <Route path="/quizzes/login" element={<h1>Login page</h1>} />
+        <Route
+          path="/quizzes/create"
+          element={
+            <ProtectedRoute>
+              <h1>Create Quiz page</h1>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
