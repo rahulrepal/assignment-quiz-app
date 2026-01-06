303977848ec5f15bc5d89e34e77875f55ebad2c6
Rahul Repal <rahulrepal@MacBookAir.bbrouter>
2026-01-06 11:59:05 +0530

    Refactor LoginPage layout and improve navigation with Navbar component

--- frontend/src/features/auth/pages/LoginPage.tsx
+++ frontend/src/features/auth/pages/LoginPage.tsx
@@ -1,25 +1,45 @@
+import { Navbar } from "@/components/shared";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useLogin } from "@/features/auth/hooks"
+import { useLogin } from "@/features/auth/hooks";
+import { ArrowLeft, ArrowRight, Lock } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export interface LoginPageProps {}
 
 export function LoginPage({}: LoginPageProps) {
-  const {
-    username,
-    setUsername,
-    password,
-    setPassword,
-    login
-  } = useLogin();
+  const navigate = useNavigate();
+  const { username, setUsername, password, setPassword, login } = useLogin();
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <main className="min-h-screen w-full flex items-center justify-center">
-        <Card className="min-w-[350px] md:min-w-[400px] h-full">
-          <CardHeader>
+      <Navbar
+        actions={[
+          <Button
+            variant="ghost"
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            <ArrowLeft className="h-4 w-4" />
+            Back to Quizzes
+          </Button>,
+        ]}
+        hideTitle
+      />
+      <main className="flex flex-1 items-center justify-center px-4">
+        <Card className="w-full max-w-md h-full">
+          <CardHeader className="space-y-1">
             <CardTitle>Login to your account</CardTitle>
+            <p className="text-sm text-muted-foreground">
+              Enter your credentials to continue
+            </p>
           </CardHeader>
           <CardContent>
             <div className="flex flex-col gap-6">
@@ -46,13 +66,16 @@ export function LoginPage({}: LoginPageProps) {
               </div>
             </div>
           </CardContent>
-          <CardFooter>
+          <CardFooter className="flex flex-col gap-3">
             <Button type="submit" className="w-full" onClick={login}>
-              Login
+              Login <ArrowRight />
             </Button>
+            <span className="text-xs text-center text-muted-foreground flex gap-2 items-start">
+              <Lock className="w-4 h-4" /> Only admins can access this panel
+            </span>
           </CardFooter>
         </Card>
       </main>
     </div>
-  )
-};
+  );
+}
